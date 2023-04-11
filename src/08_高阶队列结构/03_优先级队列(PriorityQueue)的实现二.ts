0c3722398368b1a52afe0c2daaa885e85cafb26c
wff <283953455@qq.com>
2023-04-11 16:32:39 +0800

    feat: 实现优先级队列(方案二)

---
+++ src/08_高阶队列结构/03_优先级队列(PriorityQueue)的实现二.ts
@@ -0,0 +1,53 @@
+import Heap from '../07_堆结构/06_堆结构Heap(二叉堆)'
+
+class PriorityQueue<T> {
+  private heap: Heap<T> = new Heap()
+
+  enqueue(value: T) {
+    this.heap.insert(value)
+  }
+
+  dequeue(): T | undefined {
+    return this.heap.extract()
+  }
+
+  peek(): T | undefined {
+    return this.heap.peek()
+  }
+
+  isEmpty() {
+    return this.heap.isEmpty()
+  }
+
+  size() {
+    return this.heap.size()
+  }
+
+  print() {
+    this.heap.print()
+  }
+}
+
+class Student {
+  name: string
+  score: number
+  constructor(name: string, score: number) {
+    this.name = name
+    this.score = score
+  }
+
+  valueOf() {
+    return this.score
+  }
+}
+
+const priorityQueue = new PriorityQueue()
+
+priorityQueue.enqueue(new Student('why', 99))
+priorityQueue.enqueue(new Student('kobe', 89))
+priorityQueue.enqueue(new Student('james', 95))
+priorityQueue.enqueue(new Student('curry', 88))
+
+while (!priorityQueue.isEmpty()) {
+  console.log(priorityQueue.dequeue())
+}
